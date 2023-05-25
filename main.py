a3d1d7c88ca6fabddd7d0347b4640354b45f908e
Clark Llarena <clarkllarena@ymail.com>
2023-05-25 09:33:16 +0800

    Update main.py
    replaced fig.canvas.set_window_title with fig.canvas.manager.set_window_title in the _render_display
    added check on 
    Added check for image existence in IMG_PATH before processing
    
    This commit modifies the code to include a check for the existence of the image file in the specified IMG_PATH directory before processing. If the image file doesn't exist, the code skips processing for that particular image, preventing errors or unnecessary operations.

--- main.py
+++ main.py
@@ -295,7 +295,7 @@ def draw_plot_func(dictionary, n_classes, window_title, plot_title, x_label, out
             if i == (len(sorted_values)-1): # largest bar
                 adjust_axes(r, t, fig, axes)
     # set window title
-    fig.canvas.set_window_title(window_title)
+    fig.canvas.manager.set_window_title(window_title)
     # write classes in y axis
     tick_font_size = 12
     plt.yticks(range(n_classes), sorted_keys, fontsize=tick_font_size)
@@ -701,7 +701,7 @@ with open(output_files_path + "/output.txt", 'w') as output_file:
             plt.fill_between(area_under_curve_x, 0, area_under_curve_y, alpha=0.2, edgecolor='r')
             # set window title
             fig = plt.gcf() # gcf - get current figure
-            fig.canvas.set_window_title('AP ' + class_name)
+            fig.canvas.manager.set_window_title('AP ' + class_name)
             # set plot title
             plt.title('class: ' + text)
             #plt.suptitle('This is a somewhat long figure title', fontsize=16)
@@ -742,6 +742,14 @@ if show_animation:
         img_id = tmp_file[tmp_file.find(start)+len(start):tmp_file.rfind('_ground_truth.json')]
         img_cumulative_path = output_files_path + "/images/" + img_id + ".jpg"
         img = cv2.imread(img_cumulative_path)
+        
+         # Check if the image file exists in IMG_PATH
+        img_path = IMG_PATH + '/' + img_id + ".jpg"
+        if not os.path.exists(img_path):
+            continue  # Skip processing if image file doesn't exist
+        
+        img = cv2.imread(img_cumulative_path)
+        
         if img is None:
             img_path = IMG_PATH + '/' + img_id + ".jpg"
             img = cv2.imread(img_path)
